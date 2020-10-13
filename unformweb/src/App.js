f035b9cd6deee20413eb7ff102c1c99ff73d5f26
Pedro Barão <phbarao@gmail.com>
2020-10-12 22:22:29 -0300

    adding unform project

---
+++ unformweb/src/App.js
@@ -0,0 +1,82 @@
+import React, { useRef, useEffect } from 'react';
+import { Form } from '@unform/web';
+import { Scope } from '@unform/core';
+import * as Yup from 'yup';
+import './App.css';
+
+import Input from './components/Form/input';
+
+function App() {
+  const formRef = useRef(null);
+
+  async function handleSubmit(data, { reset }) {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório'),
+        email: Yup.string()
+          .email('Digite um e-mail válido')
+          .required('O e-mail é obrigatório'),
+        address: Yup.object().shape({
+          city: Yup.string()
+            .min(3, 'No mínimo 3 caracteres')
+            .required('A cidade é obrigatório'),
+        }),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      console.log(data);
+
+      formRef.current.setErrors({});
+
+      reset();
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errorMessages = {};
+
+        err.inner.forEach((error) => {
+          errorMessages[error.path] = error.message;
+        });
+
+        formRef.current.setErrors(errorMessages);
+      }
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      formRef.current.setData({
+        name: 'Pedro',
+        email: 'pedro@teste.com.br',
+        address: {
+          city: 'Salvador',
+        },
+      });
+    }, 2000);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hello!</h1>
+
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <Input name="name" />
+        <Input type="email" name="email" />
+
+        <Scope path="address">
+          <Input name="street" />
+          <Input name="neighborhood" />
+          <Input name="city" />
+          <Input name="state" />
+          <Input name="number" />
+        </Scope>
+
+        <button type="submit">Enviar</button>
+      </Form>
+    </div>
+  );
+}
+
+export default App;
