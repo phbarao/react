a07b18aad172b1af5462628950ce105c08bd6f7c
Pedro Barão <phbarao@gmail.com>
2021-02-04 00:30:51 -0300

    adding load-data-button

---
+++ load-data-button/src/App.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+import axios from "axios";
+
+import "./App.css";
+
+function App() {
+  const [cardData, setCardData] = useState([]);
+  const [visible, setVisible] = useState(6);
+
+  const allCardData = async () => {
+    const response = await axios.get("https://randomuser.me/api/?results=36");
+
+    setCardData(response.data.results);
+  };
+
+  const loadMore = () => {
+    setVisible(visible + 6);
+  };
+
+  useEffect(() => {
+    allCardData();
+  }, []);
+
+  const renderCard = (person, index) => {
+    return (
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={person.picture.large} />
+
+        <Card.Body>
+          <Card.Title>
+            {person.name.first}
+            {person.name.last}
+          </Card.Title>
+
+          <Card.Text>
+            <ul>
+              <li>{person.email}</li>
+              <li>{person.cell}</li>
+              <li>{person.gender}</li>
+            </ul>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  };
+
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className="cards">
+          {cardData.slice(0, visible).map(renderCard)}
+        </div>
+      </div>
+      {visible < cardData.length && (
+        <button onClick={loadMore}>Load More</button>
+      )}
+    </div>
+  );
+}
+
+export default App;
